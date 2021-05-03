09fbe0efabca0737cfb93fce57d2462a163908e9
Brahmeswara <brahmeswara@yahoo.com>
2021-05-03 10:23:15 -0700

    cleanup

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,3 @@
 
 echo "Running SAM Deploy ...."
-sam deploy --template-file ./out.yml --stack-name docudata-be-lambda-layer-stack ^
---capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM
+sam deploy --template-file ./out.yml --stack-name docudata-be-lambda-layer-app-deps-stack --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_NAMED_IAM
--- package.bat
+++ package.bat
@@ -1,3 +1,2 @@
 echo "Running SAM Package...."
-sam package --template-file template.yml --s3-bucket docudata-be-sam-deploy-bucket ^
---output-template-file out.yml
+sam package --template-file template.yml --s3-bucket docudata-be-sam-deploy-bucket --output-template-file out.yml
